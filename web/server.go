fc3209ef205fa2eb6367f7c012b9c8f4ccd00db7
Durucz Béla <30567460+the-developer-guy@users.noreply.github.com>
2024-10-08 20:46:27 +0200

    Added placeholder services

--- web/server.go
+++ web/server.go
@@ -8,6 +8,29 @@ import (
 type MainPage struct {
 	PageTitle string
 	Version   string
+	Services  *[]Service
+}
+
+type Service struct {
+	Name   string
+	Url    string
+	Locked bool
+}
+
+func getServices() *[]Service {
+	services := []Service{}
+	services = append(services, Service{
+		Name:   "main branch",
+		Url:    "https://github.com/the-developer-guy/LoTo",
+		Locked: false,
+	})
+	services = append(services, Service{
+		Name:   "coffee machine",
+		Url:    "https://www.cl.cam.ac.uk/coffee/xvcoffee.html",
+		Locked: true,
+	})
+
+	return &services
 }
 
 func StartServer() {
@@ -16,6 +39,7 @@ func StartServer() {
 		data := MainPage{
 			PageTitle: "LoTo - 🔒Lockout-Tagout🏷️",
 			Version:   "1.0.0",
+			Services:  getServices(),
 		}
 		tmpl.Execute(w, data)
 	})
