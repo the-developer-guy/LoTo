b234322f416c728b0db33b9774bfb7dbf5c5b190
Durucz Béla <30567460+the-developer-guy@users.noreply.github.com>
2024-10-09 20:46:31 +0200

    Added db-config init

--- internals/db.go
+++ internals/db.go
@@ -3,6 +3,7 @@ package internals
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"os"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -39,6 +40,37 @@ func NewDatabaseHelper() (*DatabaseHelper, error) {
 }
 
 func (dh *DatabaseHelper) InitConfig(service *Service) error {
+	row := dh.Database.QueryRow("SELECT reservedBy FROM services WHERE name=?", service.Name)
+	var reservedBy sql.NullString
+	err := row.Scan(&reservedBy)
+	if err != nil {
+		err = dh.InsertService(service)
+		return err
+	}
+	if !reservedBy.Valid {
+		service.Locked = false
+		return nil
+	}
+	service.Locked = reservedBy.String != ""
+
+	return nil
+}
+
+func (dh *DatabaseHelper) InsertService(service *Service) error {
+	insert := `INSERT INTO services(name, url, reservedBy) VALUES (?, ?, ?)`
+	statement, err := dh.Database.Prepare(insert)
+	if err != nil {
+		return err
+	}
+
+	lockedBy := ""
+	if service.Locked {
+		lockedBy = "placeholder"
+	}
+	_, err = statement.Exec(service.Name, service.Url, lockedBy)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
@@ -55,5 +87,30 @@ func (dh *DatabaseHelper) dbInit() error {
 		return err
 	}
 	_, err = statement.Exec()
+	return err
+}
+
+func (dh *DatabaseHelper) dbDump() error {
+	row, err := dh.Database.Query(`SELECT name, url, reservedBy FROM services`)
+	if err != nil {
+		return err
+	}
+	defer row.Close()
+
+	for row.Next() {
+		var name, url, reservedBy sql.NullString
+		row.Scan(&name, &url, &reservedBy)
+		if name.Valid {
+			log.Print(name.String)
+		}
+		if url.Valid {
+			log.Print(url.String)
+		}
+		if reservedBy.Valid {
+			log.Print(reservedBy.String)
+		}
+		log.Println()
+
+	}
 	return err
 }
